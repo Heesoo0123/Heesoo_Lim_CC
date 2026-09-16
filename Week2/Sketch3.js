e3fb3071ac3f718277ca97582cf3780f72939cc3
Heesoo0123 <165269924+Heesoo0123@users.noreply.github.com>
2026-09-15 23:29:57 -0400

    bouncing ball

--- Week2/Sketch3.js
+++ Week2/Sketch3.js
@@ -1,20 +1,35 @@
-let x,y;
+let x;
+let y;
+let w = 100, h=100;
 let diameter = 50;
-let yVel = 5;
+let xSpeed = 5;
+let ySpeed = 10;
+
 function setup(){
    createCanvas(windowWidth, windowHeight)
-   background(100)
-
-
+   x = width/2;
+   y = 50
+   strokeWeight(5)
 }
-function draw(){
-   background(100)
-   ellipse(X,Y,diameter)
 
-   y=5
-   if(y == 5){// = just set the value, == is a comparison operator
-    y=-5
+function draw(){
+   background(100,400,140,50)
+   ellipse(x,y,w,h)
+
+   if(y > height - h/2){
+    ySpeed = -ySpeed;
    }
-  
+   if(y < h/2){
+    ySpeed = -ySpeed;
+   }
+   if(x < w/2){
+    xSpeed = -xSpeed;
+   }
+   if(x > width - w/2){
+    xSpeed = -xSpeed;
+   }
+
+   x += xSpeed;
+   y += ySpeed;
    
 }
