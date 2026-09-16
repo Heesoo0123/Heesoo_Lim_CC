d1defee6c0e9872d270761e1c0d09a025eae5442
Heesoo0123 <165269924+Heesoo0123@users.noreply.github.com>
2026-09-15 23:39:31 -0400

    transparency

--- Week2/Sketch3.js
+++ Week2/Sketch3.js
@@ -1,6 +1,6 @@
 let x;
 let y;
-let w = 100, h=100;
+let w = 100, h=20;
 let diameter = 50;
 let xSpeed = 5;
 let ySpeed = 10;
@@ -9,11 +9,12 @@ function setup(){
    createCanvas(windowWidth, windowHeight)
    x = width/2;
    y = 50
-   strokeWeight(5)
+   strokeWeight(10)
 }
 
 function draw(){
-   background(100,400,140,50)
+   background(0,30,0,10)
+   stroke(255,10,0)
    ellipse(x,y,w,h)
 
    if(y > height - h/2){
