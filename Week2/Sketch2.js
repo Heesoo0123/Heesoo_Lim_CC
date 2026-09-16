bc8197ffe3889ea05341c261d7a875725353f033
Heesoo0123 <165269924+Heesoo0123@users.noreply.github.com>
2026-09-16 00:44:15 -0400

    X/Y reverse update

--- Week2/Sketch2.js
+++ Week2/Sketch2.js
@@ -1,8 +1,9 @@
 function setup(){
    createCanvas(windowWidth, windowHeight)
    background(100,200,255)
-
-   fill(100,25,20)
+   
+   rectMode(CENTER)
+   fill(10,25,20)
    noStroke()
 
    strokeWeight(5)
@@ -10,9 +11,16 @@ function setup(){
 
 }
 function draw(){
-ellipse(mouseX,mouseY,50,50)
+stroke(150,100,0)
+fill(100,50,255,50)
+rect(mouseX,mouseY,50,50)
 ellipse(mouseX,mouseY,10,10)
-fill(100,200,255)
+
+
+stroke(204,255,0)
+fill(150,200,255,90)
+ellipse(mouseY,mouseX,150,150)
+ellipse(mouseY,mouseX,10,10)
     
 }
 
